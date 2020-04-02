10c52d2496357333ff8a95ae1095a15e8a4d7724
Michał Cieciora <mcieciora@sii.pl>
2020-04-02 17:02:11 +0200

    02042020-1702

---
+++ Command.py
@@ -0,0 +1,5 @@
+class Command:
+    def __init__(self, structure, name, usage_pattern):
+        self.structure = structure
+        self.name = name
+        self.usage_pattern = usage_pattern
---
+++ Expression.py
@@ -0,0 +1,4 @@
+class Expression:
+    def __init__(self, structure, call):
+        self.structure = structure
+        self.call = call
