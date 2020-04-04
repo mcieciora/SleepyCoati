569b1b421a35116062afcbe9f353ad512daaf2f6
mcieciora <michalcieciora96@gmail.com>
2020-04-04 17:16:58 +0200

    04042020-1716

--- reader.py
+++ reader.py
@@ -6,15 +6,27 @@ def read_co_file(file_path):
     read_output = []
     with open(file_path, 'r') as file:
         for line in file.readlines():
-            split_command_list = line.replace('\n', '').split(' ')
-            read_output.append(Expression(split_command_list[0], value_evaluation(split_command_list[1:])))
+            ret_val = validate_line(line)
+            if ret_val[0]:
+                read_output.append(ret_val[1])
     return read_output
 
 
 def value_evaluation(expression_list):
+    return_list = []
     for x in range(len(expression_list)):
-        try:
-            expression_list[x] = ast.literal_eval(expression_list[x])
-        except ValueError:
-            pass
-    return expression_list
+        if expression_list[x] != '':
+            try:
+                return_list.append(ast.literal_eval(expression_list[x]))
+            except ValueError:
+                return_list.append(expression_list[x])
+    return return_list
+
+
+def validate_line(line):
+    if line != '':
+        split_command_list = ' '.join(line.split())
+        split_command_list = split_command_list.replace('\n', '').split(' ')
+        return [True, Expression(split_command_list[0], value_evaluation(split_command_list[1:]))]
+    else:
+        return [False]
