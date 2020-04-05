0c4db74da7f8ab4d72229ba2983c12a24a525b0e
mcieciora <michalcieciora96@gmail.com>
2020-04-05 10:05:14 +0200

    05042020-1005

--- parser.py
+++ parser.py
@@ -3,17 +3,17 @@ import reader
 from Command import Command
 
 
-def build_command_list(co_file_path):
+def build_command_list(co_file_path, xml_file_path):
     commands_list = []
     for expression in reader.read_co_file(co_file_path):
-        command = get_command_by_structure(expression.head)
+        command = get_command_by_structure(expression.head, xml_file_path)
         commands_list.append(Command(expression.head, command.name, command.usage_pattern, expression.body))
     return commands_list
 
 
-def check_usage_pattern_correctness(expression_object):
+def check_usage_pattern_correctness(expression_object, xml_file_path):
     if set(get_list_elements_type(expression_object.body)) == \
-            set(get_command_by_structure(expression_object.head).usage_pattern):
+            set(get_command_by_structure(expression_object.head, xml_file_path).usage_pattern):
         return True
     return False
 
@@ -22,7 +22,7 @@ def get_list_elements_type(expression_list):
     return [type(x) for x in expression_list]
 
 
-def get_command_by_structure(structure):
-    for command in xml_parser.generate_all_commands_list('command.xml'):
+def get_command_by_structure(structure, xml_file_path):
+    for command in xml_parser.generate_all_commands_list(xml_file_path):
         if command.structure == structure:
             return command
--- xml_parser.py
+++ xml_parser.py
@@ -1,7 +1,7 @@
 import xmltodict
 
 from Command import Command
-
+from collections import OrderedDict
 
 def generate_all_commands_list(xml_file_path):
     with open(xml_file_path) as co:
@@ -11,13 +11,16 @@ def generate_all_commands_list(xml_file_path):
     if type(commands) != list:
         commands = [commands]
     for command in commands:
-        new_command = Command(command['structure'], command['name'], get_usage_patterns(command))
+        new_command = Command(command['structure'], command['name'], get_usage_patterns(command['usage_pattern']))
         all_commands.append(new_command)
     return all_commands
 
 
 def get_usage_patterns(command):
-    usage_pattern_list = command['usage_pattern']['element']
-    if type(usage_pattern_list) != list:
-        usage_pattern_list = [usage_pattern_list]
-    return [eval(element) for element in usage_pattern_list]
+    if len(command) > 0:
+        usage_pattern_list = command['element']
+        if type(usage_pattern_list) != list:
+            usage_pattern_list = [usage_pattern_list]
+        return [eval(element) for element in usage_pattern_list]
+    else:
+        return []
